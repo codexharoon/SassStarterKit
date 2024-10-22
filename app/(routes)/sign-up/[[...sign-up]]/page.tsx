d63769cb411a371a63efff6be6572c73eb249e7e
Muhammad Haroon <theharoonsocial@gmail.com>
2024-10-23 00:34:51 +0500

    completed signup with custom ui

--- app/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -6,19 +6,54 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { OAuthStrategy } from "@clerk/types";
 import { useSignIn } from "@clerk/nextjs";
+import { useSignUp } from "@clerk/nextjs";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useState } from "react";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import { useRouter } from "next/navigation";
+
+const formSchema = z.object({
+  emailAddress: z.string().email(),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters" }),
+});
+
+const FormSchema = z.object({
+  pin: z.string().min(6, {
+    message: "Your one-time password must be 6 characters.",
+  }),
+});
 
 export default function CardsCreateAccount() {
+  const [verifying, setVerifying] = useState(false);
+  const { isLoaded, signUp, setActive } = useSignUp();
+  const router = useRouter();
+
   const { signIn } = useSignIn();
 
   if (!signIn) return null;
+  if (!isLoaded) return null;
 
   const signInWith = (strategy: OAuthStrategy) => {
     return signIn.authenticateWithRedirect({
@@ -28,43 +63,189 @@ export default function CardsCreateAccount() {
     });
   };
 
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      emailAddress: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    if (!isLoaded) return;
+
+    try {
+      await signUp.create(values);
+
+      await signUp.prepareEmailAddressVerification({
+        strategy: "email_code",
+      });
+
+      // Set 'verifying' true to display second form
+      setVerifying(true);
+    } catch (error: any) {
+      console.error(JSON.stringify(error, null, 2));
+    }
+  }
+
+  // verify email :
+
+  const verifyform = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      pin: "",
+    },
+  });
+
+  async function onVerify(data: z.infer<typeof FormSchema>) {
+    if (!isLoaded) return;
+
+    try {
+      const verifyAttempt = await signUp.attemptEmailAddressVerification({
+        code: data.pin,
+      });
+
+      if (verifyAttempt.status === "complete") {
+        await setActive({
+          session: verifyAttempt.createdSessionId,
+        });
+
+        router.push("/");
+      } else {
+        console.error(JSON.stringify(verifyAttempt, null, 2));
+      }
+    } catch (error: any) {
+      console.error(JSON.stringify(error, null, 2));
+    }
+  }
   return (
-    <Card>
-      <CardHeader className="space-y-1">
-        <CardTitle className="text-2xl">Create an account</CardTitle>
-        <CardDescription>
-          Enter your email below to create your account
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-        <div className="grid grid-cols-1 gap-6">
-          <Button variant="outline" onClick={() => signInWith("oauth_google")}>
-            <Icons.google />
-            Google
-          </Button>
-        </div>
-        <div className="relative">
-          <div className="absolute inset-0 flex items-center">
-            <span className="w-full border-t" />
-          </div>
-          <div className="relative flex justify-center text-xs uppercase">
-            <span className="bg-background px-2 text-muted-foreground">
-              Or continue with
-            </span>
-          </div>
-        </div>
-        <div className="grid gap-2">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" />
-        </div>
-        <div className="grid gap-2">
-          <Label htmlFor="password">Password</Label>
-          <Input id="password" type="password" />
-        </div>
-      </CardContent>
-      <CardFooter>
-        <Button className="w-full">Create account</Button>
-      </CardFooter>
-    </Card>
+    <>
+      {!verifying ? (
+        <Card>
+          <CardHeader className="space-y-1">
+            <CardTitle className="text-2xl">Create an account</CardTitle>
+            <CardDescription>
+              Enter your email below to create your account
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent className="grid gap-4">
+            <div className="grid grid-cols-1 gap-6">
+              <Button
+                variant="outline"
+                onClick={() => signInWith("oauth_google")}
+              >
+                <Icons.google />
+                Google
+              </Button>
+            </div>
+
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">
+                  Or continue with
+                </span>
+              </div>
+            </div>
+
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-4"
+              >
+                <FormField
+                  control={form.control}
+                  name="emailAddress"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="codex@example.com" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input placeholder="" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <Button type="submit" className="w-full">
+                  Create account
+                </Button>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+      ) : (
+        <Card>
+          <CardHeader className="space-y-1">
+            <CardTitle className="text-2xl">Verify your account</CardTitle>
+            <CardDescription>
+              Check your email for a verification code
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent className="grid gap-4">
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground"></span>
+              </div>
+            </div>
+
+            <Form {...verifyform}>
+              <form
+                onSubmit={verifyform.handleSubmit(onVerify)}
+                className="w-2/3 space-y-6"
+              >
+                <FormField
+                  control={verifyform.control}
+                  name="pin"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>One-Time Password</FormLabel>
+                      <FormControl>
+                        <InputOTP maxLength={6} {...field}>
+                          <InputOTPGroup>
+                            <InputOTPSlot index={0} />
+                            <InputOTPSlot index={1} />
+                            <InputOTPSlot index={2} />
+                            <InputOTPSlot index={3} />
+                            <InputOTPSlot index={4} />
+                            <InputOTPSlot index={5} />
+                          </InputOTPGroup>
+                        </InputOTP>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <Button type="submit" className="w-full">
+                  Verify
+                </Button>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+      )}
+    </>
   );
 }
